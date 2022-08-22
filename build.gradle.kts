60e690983e4ea1992dfe4b2db4d86a521b1c1a1b
irack000 <IRACK000@gmail.com>
2022-08-22 15:47:51 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,16 @@ import org.jetbrains.compose.compose
 
 plugins {
     kotlin("multiplatform")
-    id("org.jetbrains.compose") version "1.1.0"
+    id("org.jetbrains.compose")
     id("com.android.library")
 }
-
+/*
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+*/
 group = "io.github.untactorder"
 version = "1.0"
 
@@ -32,12 +38,12 @@ kotlin {
         val androidMain by getting {
             dependencies {
                 api("androidx.appcompat:appcompat:1.4.1")
-                api("androidx.core:core-ktx:1.7.0")
+                api("androidx.core:core-ktx:1.8.0")
             }
         }
         val androidTest by getting {
             dependencies {
-                implementation("junit:junit:4.13.2")
+                implementation("junit:junit:${rootProject.extra["junit_version"]}")
             }
         }
         val desktopMain by getting {
@@ -50,14 +56,68 @@ kotlin {
 }
 
 android {
-    compileSdk = 33
-    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    compileSdk = rootProject.extra["android_target_sdk_version"] as Int?
+    buildToolsVersion = rootProject.extra["android_build_tool_version"] as String
+    sourceSets {
+        getByName("main") {
+            // AndroidClient - UntactOrder
+            manifest.srcFile("src/androidMain/AndroidManifest.xml")
+            kotlin.srcDirs("src/androidMain/kotlin", "src/commonMain/kotlin")
+            res.srcDirs("src/androidMain/resources", "src/commonMain/resources")
+        }
+    }
     defaultConfig {
-        minSdkVersion(24)
-        targetSdkVersion(31)
+        minSdk = rootProject.extra["android_min_sdk_version"] as Int?
+        targetSdk = rootProject.extra["android_target_sdk_version"] as Int?
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility(JavaVersion.VERSION_1_8)
+        targetCompatibility(JavaVersion.VERSION_1_8)
     }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.3.0-beta01"
+    }
+    packagingOptions {
+        resources.excludes += "/META-INF/{AL2.0,LGPL2.1}"
+    }
+}
+
+dependencies {
+    // Kotlin Dependencies
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0-RC")
+
+    // Android Basic Dependencies
+    implementation("androidx.core:core-ktx:1.8.0")
+
+    // Compose Dependencies
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
+    implementation("androidx.activity:activity-compose:1.5.1")
+    implementation("androidx.compose.ui:ui:${rootProject.extra["compose_version"]}")
+    implementation("androidx.compose.ui:ui-tooling-preview:${rootProject.extra["compose_version"]}")
+    implementation("androidx.compose.material3:material3:1.0.0-alpha15")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:${rootProject.extra["compose_version"]}")
+    debugImplementation("androidx.compose.ui:ui-tooling:${rootProject.extra["compose_version"]}")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:${rootProject.extra["compose_version"]}")
+
+    implementation("io.github.shashank02051997:FancyToast:2.0.1")
+
+    // Markdown - https://halilibo.com/compose-richtext/
+    implementation("com.halilibo.compose-richtext:richtext-commonmark:0.13.0")
+    implementation("com.halilibo.compose-richtext:richtext-ui-material3:0.13.0")
+
+    // JUnit Dependencies
+    testImplementation("org.junit.jupiter:junit-jupiter-api:${rootProject.extra["junit_version"]}")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${rootProject.extra["junit_version"]}")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:${rootProject.extra["junit_version"]}")
+    testImplementation("junit:junit:4.13.2")
 }
